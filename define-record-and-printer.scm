6e313da2f2937cb9ca9804b6cc0ae7153c241568
Peter Danenberg <pcd@roxygen.org>
2012-10-10 14:03:21 -0700

    Move to core.

--- define-record-and-printer.scm
+++ define-record-and-printer.scm
@@ -4,19 +4,4 @@
   (import-for-syntax matchable)
   (use lolevel)
 
-  (define-syntax define-record-and-printer
-    (lambda (expression rename compare)
-      (match expression
-        ((_ record . fields)
-         (let ((%define-record (rename 'define-record))
-               (%define-record-printer (rename 'define-record-printer))
-               (%begin (rename 'begin))
-               (%lambda (rename 'lambda))
-               (%write (rename 'write))
-               (%record->vector (rename 'record->vector)))
-           `(,%begin
-             (,%define-record ,record ,@fields)
-             (,%define-record-printer
-              ,record
-              (,%lambda (record out)
-                   (,%write (,%record->vector record) out))))))))))
+  (include "define-record-and-printer-core.scm"))
