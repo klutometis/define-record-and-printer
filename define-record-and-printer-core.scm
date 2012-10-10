997a9f9e0d1888eef581f8e1195fe9f5b02346b7
Peter Danenberg <pcd@roxygen.org>
2012-10-10 14:03:09 -0700

    Documented core and release info

---
+++ define-record-and-printer-core.scm
@@ -0,0 +1,16 @@
+(define-syntax define-record-and-printer
+  (lambda (expression rename compare)
+    (match expression
+      ((_ record . fields)
+       (let ((%define-record (rename 'define-record))
+             (%define-record-printer (rename 'define-record-printer))
+             (%begin (rename 'begin))
+             (%lambda (rename 'lambda))
+             (%write (rename 'write))
+             (%record->vector (rename 'record->vector)))
+         `(,%begin
+           (,%define-record ,record ,@fields)
+           (,%define-record-printer
+            ,record
+            (,%lambda (record out)
+                 (,%write (,%record->vector record) out)))))))))
